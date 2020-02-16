6b7cdd5495afa9a385bad47140f8d567cb246b11
Michael Kirkpatrick <michael@mekstudios.com>
2020-02-16 09:16:07 -0800

    Updated generate-uuid.php - added validateUUID() + revised generateUUID()

--- generate-uuid.php
+++ generate-uuid.php
@@ -16,22 +16,22 @@ function generateUUID(){
 		See Section 4.4 of RFC 4122
 		--*/
 		switch($i){
-			case 6:
+			case 49:
 				$string .= '0';
 			break;
-			case 7:
+			case 50:
 				$string .= '1';
 			break;
-			case 12:
+			case 51:
 				$string .= '0';
 			break;
-			case 13:
+			case 52:
+				$string .= '0';
+			break;
+			case 65:
 				$string .= '1';
 			break;
-			case 14:
-				$string .= '0';
-			break;
-			case 15:
+			case 66:
 				$string .= '0';
 			break;
 			default:
@@ -66,8 +66,48 @@ function generateUUID(){
 			$bitCount++;
 		}
 	}
+
 	return $uuid;
 }
 
-echo generateUUID();
+function validateUUID($uuid){
+	// Default
+	$valid = false;
+	
+	// Validate UUID
+	if(preg_match('/^([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/', $uuid)){
+		// Check Version Number
+		$time_hi_and_version_hex = substr($uuid, 14, 4);
+		$time_hi_and_version_binary = sprintf("%016d", base_convert($time_hi_and_version_hex, 16, 2));
+		
+		// Check Reserved Sequence
+		$clock_seq_hi_and_reserved_hex = substr($uuid, 19, 2);
+		$clock_seq_hi_and_reserved_binary = sprintf("%08d", base_convert($clock_seq_hi_and_reserved_hex, 16, 2));
+		
+		// Validate that the two most significant bits (bits 6 and 7) of clock_seq_hi_and_reserved are zero and one, respectively.
+		if(substr($clock_seq_hi_and_reserved_binary, 0, 2) === '10'){
+			// Validate that the four most significant bits (bits 12 through 15) of the time_hi_and_version field are 0010, respectively
+			if(substr($time_hi_and_version_binary, 0, 4) === '0100'){
+				// Valid RFC 4122 v4 UUID
+				$valid = true;
+			}
+		}
+	}
+	   
+	// Return
+	return $valid;
+}
+
+// Generate a new UUID
+echo "Generating UUID...\n";
+$uuid = generateUUID();
+echo "$uuid\n";
+
+// Validate UUID
+$valid = validateUUID($uuid);
+if($valid){
+	echo "Valid UUID\n";
+}else{
+	echo "Invalid UUID\n";
+}
 ?>
